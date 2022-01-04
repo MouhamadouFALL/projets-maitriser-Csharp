ab38bbfdcd65d3d94a06f984ab3d358087a2c8d3
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-01-04 23:02:17 +0000

    Ajout d'un formulaire Client

---
+++ Projet18-managerCustomer/Projet18-managerCustomer/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet18_managerCustomer
+{
+    internal class Customer
+    {
+        private int code;
+        private string nom;
+        private string prenom;
+        private double solde;
+        private string naissance;
+        private string nationatile;
+        private string niveau;
+
+        // Constructeur
+        public Customer() {} // Sans Argument
+
+
+        
+        //////////////////////////////////////////     Getter qnd Setters      /////////////////////////////////////
+        public int Id
+        { 
+            get { return code; } 
+            set { code = value; } 
+        }
+
+        public string Nom
+        { get { return nom; } set { nom = value; } }
+
+        public string Prenom
+        { get { return prenom; } set { prenom = value; } }
+
+        public double Solde
+        { get { return solde; } set { solde = value; } }
+
+        public string Naissance
+        { get { return naissance; } set { naissance = value; } }
+
+        public string Nationatile
+        { get { return nationatile; } set { nationatile = value; } }
+
+        public string Niveau
+        { get { return niveau; } set { niveau = value; } }
+
+
+
+    }
+}
