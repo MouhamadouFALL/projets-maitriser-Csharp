fa760f05a6e82396c3aac14af6bd31b47c063a43
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-01-05 20:41:00 +0000

    Ajouter la base de données Mysql dans projet

---
+++ Projet18-managerCustomer/Client.Designer.cs
@@ -0,0 +1,456 @@
+﻿namespace Projet18_managerCustomer
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.niveau = new System.Windows.Forms.TextBox();
+            this.nationalite = new System.Windows.Forms.TextBox();
+            this.naissance = new System.Windows.Forms.TextBox();
+            this.solde = new System.Windows.Forms.TextBox();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.id = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnEffacer = new System.Windows.Forms.Button();
+            this.rniveau = new System.Windows.Forms.TextBox();
+            this.rnationalite = new System.Windows.Forms.TextBox();
+            this.rnaissance = new System.Windows.Forms.TextBox();
+            this.rsolde = new System.Windows.Forms.TextBox();
+            this.rprenom = new System.Windows.Forms.TextBox();
+            this.rnom = new System.Windows.Forms.TextBox();
+            this.rid = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnQuit);
+            this.panel1.Controls.Add(this.btnAnnuler);
+            this.panel1.Controls.Add(this.btnValider);
+            this.panel1.Controls.Add(this.niveau);
+            this.panel1.Controls.Add(this.nationalite);
+            this.panel1.Controls.Add(this.naissance);
+            this.panel1.Controls.Add(this.solde);
+            this.panel1.Controls.Add(this.prenom);
+            this.panel1.Controls.Add(this.nom);
+            this.panel1.Controls.Add(this.id);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(295, 455);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(10, 378);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(98, 40);
+            this.btnQuit.TabIndex = 17;
+            this.btnQuit.Text = "Quitter";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(192, 309);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(99, 39);
+            this.btnAnnuler.TabIndex = 16;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(10, 310);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(98, 38);
+            this.btnValider.TabIndex = 15;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
+            // 
+            // niveau
+            // 
+            this.niveau.Location = new System.Drawing.Point(192, 232);
+            this.niveau.Name = "niveau";
+            this.niveau.Size = new System.Drawing.Size(100, 20);
+            this.niveau.TabIndex = 14;
+            // 
+            // nationalite
+            // 
+            this.nationalite.Location = new System.Drawing.Point(192, 202);
+            this.nationalite.Name = "nationalite";
+            this.nationalite.Size = new System.Drawing.Size(100, 20);
+            this.nationalite.TabIndex = 13;
+            // 
+            // naissance
+            // 
+            this.naissance.Location = new System.Drawing.Point(192, 177);
+            this.naissance.Name = "naissance";
+            this.naissance.Size = new System.Drawing.Size(100, 20);
+            this.naissance.TabIndex = 12;
+            // 
+            // solde
+            // 
+            this.solde.Location = new System.Drawing.Point(192, 148);
+            this.solde.Name = "solde";
+            this.solde.Size = new System.Drawing.Size(100, 20);
+            this.solde.TabIndex = 11;
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(192, 114);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(100, 20);
+            this.prenom.TabIndex = 10;
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(192, 82);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(100, 20);
+            this.nom.TabIndex = 9;
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(192, 56);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(100, 20);
+            this.id.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1, 232);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Niveau";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(57, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Nationalité";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(2, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Date Naissance";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Solde";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Prenom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulaire Cilent";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnEffacer);
+            this.panel2.Controls.Add(this.rniveau);
+            this.panel2.Controls.Add(this.rnationalite);
+            this.panel2.Controls.Add(this.rnaissance);
+            this.panel2.Controls.Add(this.rsolde);
+            this.panel2.Controls.Add(this.rprenom);
+            this.panel2.Controls.Add(this.rnom);
+            this.panel2.Controls.Add(this.rid);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label13);
+            this.panel2.Controls.Add(this.label14);
+            this.panel2.Controls.Add(this.label15);
+            this.panel2.Controls.Add(this.label16);
+            this.panel2.Location = new System.Drawing.Point(297, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(303, 455);
+            this.panel2.TabIndex = 18;
+            // 
+            // btnEffacer
+            // 
+            this.btnEffacer.Location = new System.Drawing.Point(55, 310);
+            this.btnEffacer.Name = "btnEffacer";
+            this.btnEffacer.Size = new System.Drawing.Size(111, 38);
+            this.btnEffacer.TabIndex = 17;
+            this.btnEffacer.Text = "Effacer";
+            this.btnEffacer.UseVisualStyleBackColor = true;
+            // 
+            // rniveau
+            // 
+            this.rniveau.Location = new System.Drawing.Point(192, 232);
+            this.rniveau.Name = "rniveau";
+            this.rniveau.Size = new System.Drawing.Size(100, 20);
+            this.rniveau.TabIndex = 14;
+            // 
+            // rnationalite
+            // 
+            this.rnationalite.Location = new System.Drawing.Point(192, 202);
+            this.rnationalite.Name = "rnationalite";
+            this.rnationalite.Size = new System.Drawing.Size(100, 20);
+            this.rnationalite.TabIndex = 13;
+            // 
+            // rnaissance
+            // 
+            this.rnaissance.Location = new System.Drawing.Point(192, 177);
+            this.rnaissance.Name = "rnaissance";
+            this.rnaissance.Size = new System.Drawing.Size(100, 20);
+            this.rnaissance.TabIndex = 12;
+            // 
+            // rsolde
+            // 
+            this.rsolde.Location = new System.Drawing.Point(192, 148);
+            this.rsolde.Name = "rsolde";
+            this.rsolde.Size = new System.Drawing.Size(100, 20);
+            this.rsolde.TabIndex = 11;
+            // 
+            // rprenom
+            // 
+            this.rprenom.Location = new System.Drawing.Point(192, 114);
+            this.rprenom.Name = "rprenom";
+            this.rprenom.Size = new System.Drawing.Size(100, 20);
+            this.rprenom.TabIndex = 10;
+            // 
+            // rnom
+            // 
+            this.rnom.Location = new System.Drawing.Point(192, 82);
+            this.rnom.Name = "rnom";
+            this.rnom.Size = new System.Drawing.Size(100, 20);
+            this.rnom.TabIndex = 9;
+            // 
+            // rid
+            // 
+            this.rid.Location = new System.Drawing.Point(192, 56);
+            this.rid.Name = "rid";
+            this.rid.Size = new System.Drawing.Size(100, 20);
+            this.rid.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1, 232);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Niveau";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(4, 205);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(57, 13);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "Nationalité";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(2, 177);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(83, 13);
+            this.label11.TabIndex = 5;
+            this.label11.Text = "Date Naissance";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(4, 148);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(34, 13);
+            this.label12.TabIndex = 4;
+            this.label12.Text = "Solde";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(3, 114);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(43, 13);
+            this.label13.TabIndex = 3;
+            this.label13.Text = "Prenom";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(3, 84);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(29, 13);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "Nom";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(4, 56);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(18, 13);
+            this.label15.TabIndex = 1;
+            this.label15.Text = "ID";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(100, 9);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(66, 13);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "Recapitulatif";
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(599, 455);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Button btnValider;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnEffacer;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        public System.Windows.Forms.TextBox niveau;
+        public System.Windows.Forms.TextBox nationalite;
+        public System.Windows.Forms.TextBox naissance;
+        public System.Windows.Forms.TextBox solde;
+        public System.Windows.Forms.TextBox prenom;
+        public System.Windows.Forms.TextBox nom;
+        public System.Windows.Forms.TextBox id;
+        public System.Windows.Forms.TextBox rniveau;
+        public System.Windows.Forms.TextBox rnationalite;
+        public System.Windows.Forms.TextBox rnaissance;
+        public System.Windows.Forms.TextBox rsolde;
+        public System.Windows.Forms.TextBox rprenom;
+        public System.Windows.Forms.TextBox rnom;
+        public System.Windows.Forms.TextBox rid;
+    }
+}
---
+++ Projet18-managerCustomer/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projet18_managerCustomer
+{
+    public partial class Client : Form
+    {
+
+        public Client()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            Customer client = new Customer();
+
+            client.Id = int.Parse(id.Text);
+            client.Nom = nom.Text;
+            client.Prenom = prenom.Text;
+            client.Solde = double.Parse(solde.Text);
+            client.Naissance = naissance.Text;
+            client.Nationatile = nationalite.Text;
+            client.Niveau = niveau.Text;
+
+            rid.Text = client.Id.ToString();
+            rnom.Text = client.Nom;
+            rprenom.Text = client.Prenom;
+            rsolde.Text = client.Solde.ToString();
+            rnaissance.Text = client.Naissance;
+            rnationalite.Text = client.Nationatile;
+            rniveau.Text = client.Niveau;
+
+            DBConnect dBConnect = new DBConnect();
+            dBConnect.Insert();
+        }
+    }
+}
