42c1973fd7401b946172487067608c8f08993640
MouhamadouFALL <mouhamadou2.fall@gmail.com>
2022-01-06 04:02:02 +0000

    Commenter la classe DBCommect

--- Projet18-managerCustomer/Projet18-managerCustomer/DBConnect.cs
+++ Projet18-managerCustomer/Projet18-managerCustomer/DBConnect.cs
@@ -12,8 +12,8 @@ namespace Projet18_managerCustomer
     internal class DBConnect
     {
 
-        private string nomUser;
-        private string nomPassword;
+        private string user;
+        private string password;
         private string dbName;
         private string server;
         private MySqlConnection conn;
@@ -27,18 +27,20 @@ namespace Projet18_managerCustomer
         // Créer la connexion à la base de données
         private void initilize()
         {
-            nomUser = "root";
-            nomPassword = "";
+            user = "root";
+            password = "";
             dbName = "client_ms";
             server = "localhost";
+            
             string connexion = "SERVER=" + server + ";" 
                 + "DATABASE=" + dbName + ";" 
-                + "UID=" + nomUser + ";" 
-                + "PASSWORD=" + nomPassword + ";";
+                + "UID=" + user + ";" 
+                + "PASSWORD=" + password + ";";
 
             conn = new MySqlConnection(connexion);
         }
 
+        // ouvrir une connexion à la base de données client_ms
         private bool OpenConnexion()
         {
             try
@@ -46,14 +48,13 @@ namespace Projet18_managerCustomer
                 conn.Open();
                 return true;
 
-
             }catch (MySqlException ex)
             {
                 switch(ex.Number)
                 {
-                    case 0: MessageBox.Show("Problemede connnexion");
+                    case 0: MessageBox.Show("Problème de connnexion !!");
                         break;
-                    case 1045: MessageBox.Show("Login ou Mot de pass incorrecte");
+                    case 1045: MessageBox.Show("Login ou Mot de pass incorrecte !");
                         break;
                 }
 
@@ -63,14 +64,15 @@ namespace Projet18_managerCustomer
 
         public void Insert(int code, string nom, string prenom, int solde, string naissance, string nationalite, string niveau)
         {
+            // définir la requête 
             string request = "INSERT INTO customers(code, nom, prenom, solde, naissance, nationalite, niveau)" 
                 + " VALUES("+code+", '"+ nom + "', '" + prenom + "', '" + solde + "', '" + naissance + "', '" + nationalite + "', '" + niveau + "')";
 
-            if (this.OpenConnexion() == true)
+            if (this.OpenConnexion() == true) // vérifier si la connexion à été ouvert
             {
                 MySqlCommand cmd = new MySqlCommand(request, conn);
-                cmd.ExecuteNonQuery();
-                this.conn.Close();
+                cmd.ExecuteNonQuery(); // Exécter la requpete
+                this.conn.Close(); // fermer la session de connexion
             }
         }
 
